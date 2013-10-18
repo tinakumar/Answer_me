c06361872f433ec48c5d7c14e435dc090fec69c2
Tina Kumar <tinakumar07@gmail.com>
2013-10-17 22:59:09 -0700

    Added ReadMe

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,7 @@ class QuestionsController < ApplicationController
   # POST /questions.json
   def create
     @question = Question.new(question_params)
-    @question.user_id = current_user.id
+    #@question.user_id = current_user.id
 
   respond_to do |format|
       if @question.save
--- db/migrate/20131017203834_create_join_table_question_user.rb
+++ db/migrate/20131017203834_create_join_table_question_user.rb
@@ -1,8 +1,10 @@
 class CreateJoinTableQuestionUser < ActiveRecord::Migration
-  def change
-    create_join_table :questions, :users do |t|
-      t.index [:question_id, :user_id]
-      t.index [:user_id, :question_id]
+    def change
+      create_join_table :questions, :users do |t|
+        t.index [:question_id, :user_id]
+        t.index [:user_id, :question_id]
     end
   end
 end
+
+
