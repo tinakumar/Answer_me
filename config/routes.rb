39c461bcd4d2c4075940d198e10ebabfb1d9e17a
Tina Kumar <tinakumar07@gmail.com>
2013-10-21 18:22:24 -0700

    fix view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AnswerMe::Application.routes.draw do
   resources :questions
 
+  resources :questions_users
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
